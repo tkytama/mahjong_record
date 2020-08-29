a8a1d101d332205b12bf1067287ac85dfe6f67af
tkytama <itotamanegi@gmail.com>
2020-08-29 14:47:35 +0900

    トップページの生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'mahjong_record/top'
   devise_for :users
+  root to: 'mahjong_record#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   if Rails.env.development?
