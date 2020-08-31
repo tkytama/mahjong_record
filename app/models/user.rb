69651f13e1821ea832ea9ffb5ca5f574fcf2107f
tkytama <itotamanegi@gmail.com>
2020-09-01 00:01:29 +0900

    user・roomテーブルの追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
+  belongs_to :room
+  has_many :game_records
 end
