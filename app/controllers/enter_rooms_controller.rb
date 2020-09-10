341fece7fb283f764fe9f1712b56c1ab4a40bc02
tkytama <itotamanegi@gmail.com>
2020-09-10 21:26:40 +0900

    部屋入室の際にuserにroom_id記載

--- app/controllers/enter_rooms_controller.rb
+++ app/controllers/enter_rooms_controller.rb
@@ -5,8 +5,13 @@ class EnterRoomsController < ApplicationController
 
   def create
     @room = Room.find_by(room_key: room_params[:room_key])
-    redirect_to controller: :rooms, action: :show, id: @room.id 
-    
+    user = User.find(current_user.id)
+    users = User.where(room_id: @room.id)
+    if users.count < 4 && users.select(:user_id) != current_user.id 
+      user.room_id = @room.id
+      user.save
+      redirect_to controller: :rooms, action: :show, id: @room.id 
+    end
 
   end
 
