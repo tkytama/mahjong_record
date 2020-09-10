2704c970d1f529d2e8cd68d262ed7c54b36f9052
tkytama <itotamanegi@gmail.com>
2020-09-10 21:52:00 +0900

    対局画面のroutes調整とcontroller、viewの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'games/index'
+  get 'games/show'
+  get 'games/new'
+  get 'games/edit'
+  get 'games/update'
+  get 'games/destroy'
   get 'users/show'
   get 'mahjong_record/top'
   root to: 'mahjong_record#top'
@@ -10,6 +16,20 @@ Rails.application.routes.draw do
     end
   end
   
+  resources :rooms, only: [:new, :create, :show] do
+    member do
+      get :enter
+      put :entering      
+      get :game_records
+      put :input_game_records
+    end
+
+    resources :games, only:[:new, :create, :edit, :update, :index, :delete]
+  end
+
+  resources :enter_rooms, only: [:new, :create]
+
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   if Rails.env.development?
