2862ffd2b862ba21f4c061d0ef1c45614fc6a93a
tkytama <itotamanegi@gmail.com>
2020-09-13 23:03:17 +0900

    対局画面のプレイヤー表示位置

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,19 @@
 class GamesController < ApplicationController
   def index
-    @players = User.where(room_id: params[:room_id])
+    @game_records = GameRecord.where(room_id: 18)
+    @game_records_now = @game_records.where(calculation: nil)
+    @player = @game_records_now.find_by(user_id: current_user.id)
+    @player_mine = User.find(current_user.id)
+
+    if @player.seat == "ton"
+      other_player("sha", "pe", "nan")
+    elsif @player.seat == "nan"
+      other_player("pe", "ton", "sha")
+    elsif @player.seat == "sha"
+      other_player("sha", "nan", "pe")
+    else
+      other_player("nan", "sha", "ton")
+    end
   end
 
   def show
@@ -17,4 +30,16 @@ class GamesController < ApplicationController
 
   def destroy
   end
+
+ 
+    def player_direction(direction)
+      @game_records_now.find_by(seat: direction)
+    end
+
+    def other_player(front, left, right)
+      @player_front = User.find(player_direction(front).user_id)
+      @player_left = User.find(player_direction(left).user_id)
+      @player_right = User.find(player_direction(right).user_id)
+    end
+
 end
