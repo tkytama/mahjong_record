454124df241144eef2bc504691edaa322690cf0f
tkytama <itotamanegi@gmail.com>
2020-10-15 00:31:07 +0900

    焼き鳥確認

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -24,23 +24,9 @@ class GamesController < ApplicationController
   end
 
   def edit
-    @room = Room.find(params[:room_id])
     @game_records = GameRecord.where(room_id: params[:room_id])
     @game_records_now = @game_records.where(calculation: nil)
     @count = @game_records_now[0].count  
-    @player = @game_records_now.find_by(user_id: current_user.id)
-    @player_mine = User.find(current_user.id)
-
-    if @player.seat == "ton"
-      other_player("sha", "pe", "nan")
-    elsif @player.seat == "nan"
-      other_player("pe", "ton", "sha")
-    elsif @player.seat == "sha"
-      other_player("ton", "nan", "pe")
-    else
-      other_player("nan", "sha", "ton")
-    end
-    @yakitori_count = @game_records_now.where(yakitori: true).count
   end
 
   def update
@@ -54,7 +40,7 @@ class GamesController < ApplicationController
     @game_records = GameRecord.where(room_id: params[:room_id])
     @game_records_now = @game_records.where(calculation: nil)
     @count = @game_records_now[0].count  
-
+    @yakitori_count = @game_records_now.pluck(:yakitori).count(true)
 
     @player = @game_records_now.find_by(user_id: current_user.id)
     @player_mine = User.find(current_user.id)
@@ -68,10 +54,60 @@ class GamesController < ApplicationController
     else
       other_player("nan", "sha", "ton")
     end
-    @yakitori_count = @game_records_now.where(yakitori: true).count
 
+    if @room.yakitori 
+      if @front_seat.yakitori 
+        @yakitori_player ||= @front_seat.user.name
+        if @yakitori_player != nil
+           @yakitori_player << "、#{@front_seat.user.name}"
+        end
+      elsif @left_seat.yakitori 
+        @yakitori_player ||= @left_seat.user.name
+        if @yakitori_player != nil
+           @yakitori_player << "、#{@left_seat.user.name}"
+        end
+       
+      elsif @right_seat.yakitori 
+        @yakitori_player ||= @right_seat.user.name
+        if @yakitori_player != nil
+           @yakitori_player << "、#{@right_seat.user.name}"
+        end
+      else @player.yakitori 
+        @yakitori_player ||= @player.user.name
+        if @yakitori_player != nil
+           @yakitori_player << "、#{@player.user.name}"
+        end
+      end
+     end
   end
 
+  def input_tobi_yakitori
+    @room = Room.find(params[:room_id])
+    @game_records = GameRecord.where(room_id: params[:room_id])
+    @game_records_now = @game_records.where(calculation: nil)
+    @player_seat = @game_records_now.find_by(user_id: current_user.id)
+    @player_mine = User.find(current_user.id)
+
+    if @player_seat.seat == "ton"
+      other_player("sha", "pe", "nan")
+    elsif @player_seat.seat == "nan"
+      other_player("pe", "ton", "sha")
+    elsif @player_seat.seat == "sha"
+      other_player("ton", "nan", "pe")
+    else
+      other_player("nan", "sha", "ton")
+    end
+
+    @count = 0
+    seats = @room.game_records.pluck(:seat)
+    if seats.include?("ton") && seats.include?("nan") && seats.include?("sha") && seats.include?("pe")
+      if @room.update(update_room_params)
+        redirect_to "/rooms/#{@room.id}/games"
+      else
+        render 'rooms/game_records'
+      end
+    end
+  end
 
   private
  
@@ -79,10 +115,17 @@ class GamesController < ApplicationController
       @game_records_now.find_by(seat: direction)
     end
 
+    def update_room_params
+      params.require(:room).permit(game_records_attributes: [:point, :seat, :id])
+    end
+
     def other_player(front, left, right)
+      @front_seat = player_direction(front)
+      @left_seat = player_direction(left)
+      @right_seat = player_direction(right)
       @player_front = User.find(player_direction(front).user_id)
       @player_left = User.find(player_direction(left).user_id)
       @player_right = User.find(player_direction(right).user_id)
     end
 
-end
+  end
