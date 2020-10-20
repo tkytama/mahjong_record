02f46440507214a1030f56d88695bfdb1771d226
tkytama <itotamanegi@gmail.com>
2020-10-20 21:21:45 +0900

    東南西北バリデーション

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,14 +1,43 @@
-class MochiValidator < ActiveModel::EachValidator
-  def validates_each(record,after,value)
-    record.errors.add(after,'テスト') if value.include?("25000")
-  end
-end
-
 class Room < ApplicationRecord
+  TOTAL_POINT = 100000
   has_many :users
   has_many :game_records
   accepts_nested_attributes_for :game_records
   
-  validates :mochi, mochi: true
+  validate :require_total_point
+  validate :require_player_seat
+
+  def require_total_point
+    total_point = 0
+    game_records
+      .select{ |game_record| game_record.calculation == nil }
+      .each do |game_records|
+      total_point += game_records.point
+    end
+
+    errors.add(:base, '合計得点が10万点になっていません') if total_point != TOTAL_POINT
+  end
+
+  def require_player_seat
+    count_ton = 0
+    count_nan = 0
+    count_sha = 0
+    count_pe = 0
+    game_records
+      .select{ |game_record| game_record.calculation == nil }
+      .each do |game_records|
+      if game_records.seat = "ton"
+        count_ton += 1
+      elsif game_records.seat = "nan"
+        count_nan += 1
+      elsif game_records.seat = "sha"
+	count_sha += 1
+      else
+	count_pe += 1
+      end
+    end
+    errors.add(:base, '東、南、西、北を一回ずつ入力してください') if count_nan == 0 || count_ton == 0 || count_sha == 0 || count_pe == 0
+
+  end
 
 end
