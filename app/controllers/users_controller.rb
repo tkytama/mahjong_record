afdadf408d679beed65fd2c64e4c187fda42f97d
tkytama <itotamanegi@gmail.com>
2020-10-28 01:44:57 +0900

    マイページの表の表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
+    @gamerecords = GameRecord.where(user: current_user.id)
+    @rooms_number = @gamerecords.pluck(:room_id).uniq
   end
 
   def point
