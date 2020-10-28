78e93fd0c65a265fe8e70b693da6577b97428cd8
tkytama <itotamanegi@gmail.com>
2020-10-29 00:19:12 +0900

    ransack導入とpoint画面の変更

--- app/models/game_record.rb
+++ app/models/game_record.rb
@@ -1,4 +1,24 @@
 class GameRecord < ApplicationRecord
   belongs_to :user
   belongs_to :room
+
+
+
+
+  scope :created_after, -> (time) {
+    time = time.to_time
+    where('created_at > ?', time)
+  }
+  scope :created_before, -> (time) {
+    time = time.to_time
+    where('created_before > ?', time)
+  }
+
+  class << self
+    def ransackable_scopes(auth_object = nil)
+      [:created_after, :created_before]
+    end
+  end
+
+
 end
