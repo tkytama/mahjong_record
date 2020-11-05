12ed44078ad476e83407c544c4353b38894f4a4b
tkytama <itotamanegi@gmail.com>
2020-11-05 20:25:26 +0900

    devise twitterAPI

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,16 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable, :confirmable,
+	 :omniauthable, omniauth_providers: %i[twitter]
   belongs_to :room, optional: true
   has_many :game_records
   has_one_attached :image
+  
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
