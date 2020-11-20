fcd44a59665b88f2a13ce7c08310ab5138919084
tkytama <itotamanegi@gmail.com>
2020-11-21 04:30:48 +0900

    部屋作成

--- app/models/room.rb
+++ app/models/room.rb
@@ -10,10 +10,14 @@ class Room < ApplicationRecord
 
   def require_total_point
     total_point = 0
-    game_records
-      .select{ |game_record| game_record.calculation == nil }
-      .each do |game_records|
-      total_point += game_records.point
+    if game_records[0] != nil
+      game_records
+        .select{ |game_record| game_record.calculation == nil }
+        .each do |game_records|
+        total_point += game_records.point
+      end
+    else
+      total_point = TOTAL_POINT
     end
 
     errors.add(:base, '合計得点が10万点になっていません') if total_point != TOTAL_POINT
@@ -24,18 +28,25 @@ class Room < ApplicationRecord
     count_nan = 0
     count_sha = 0
     count_pe = 0
-    game_records
-      .select{ |game_record| game_record.calculation == nil }
-      .each do |game_records|
-      if game_records.seat == "ton"
-        count_ton += 1
-      elsif game_records.seat == "nan"
-        count_nan += 1
-      elsif game_records.seat == "sha"
-	count_sha += 1
-      else
-	count_pe += 1
+    if game_records[0] != nil
+      game_records
+        .select{ |game_record| game_record.calculation == nil }
+        .each do |game_records|
+        if game_records.seat == "ton"
+          count_ton += 1
+        elsif game_records.seat == "nan"
+          count_nan += 1
+        elsif game_records.seat == "sha"
+   	  count_sha += 1
+        else
+  	  count_pe += 1
+        end
       end
+    else
+      count_ton = 1
+      count_nan = 1
+      count_sha = 1
+      count_pe = 1
     end
     
     errors.add(:base, '東、南、西、北を一回ずつ入力してください') if count_nan == 0 || count_ton == 0 || count_sha == 0 || count_pe == 0
